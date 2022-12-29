1e3690e9d9905e8eca166c5e4f002e7a6a1de001
nikhil vanamala <nickhil215@gmail.com>
2022-12-29 15:59:04 +0530

    trail 4

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -37,82 +37,82 @@ public class MovieRepository {
     HashMap<String,Director> directors=new HashMap<>();
     HashMap<String,List<String>> pair=new HashMap<>();
 
- String addMovie(Movie movie){
-     String mv=movie.getName();
-    movies.put(mv,movie);
-    return "Successfully Added Movie";
-}
-String addDirector(Director director){
-     directors.put(director.getName(),director);
-     return "Successfully Added Director";
-}
-Movie getmovie(String name){
-    return movies.get(name);
+    String addMovie(Movie movie){
+        String mv=movie.getName();
+        movies.put(mv,movie);
+        return "Successfully Added Movie";
+    }
+    String addDirector(Director director){
+        directors.put(director.getName(),director);
+        return "Successfully Added Director";
+    }
+    Movie getmovie(String name){
+        return movies.get(name);
 
-}
-Director getdirecor(String name){
-    return directors.get(name);
-}
+    }
+    Director getdirecor(String name){
+        return directors.get(name);
+    }
 
 
     String addpair(String mvName,String dirName){
-    if(movies.containsKey(mvName) && directors.containsKey(dirName)){
-        if(pair.containsKey(dirName)){
-            pair.get(dirName).add(mvName);
-        }else {
-            ArrayList<String> mv=new ArrayList<>();
-            mv.add(mvName);
-            pair.put(dirName,mv);
-        }
+        if(movies.containsKey(mvName) && directors.containsKey(dirName)){
+            if(pair.containsKey(dirName)){
+                pair.get(dirName).add(mvName);
+            }else {
+                ArrayList<String> mv=new ArrayList<>();
+                mv.add(mvName);
+                pair.put(dirName,mv);
+            }
 
-    }
-     return "SUCCESSFULLY PAIRED";
+        }
+        return "SUCCESSFULLY PAIRED";
 
     }
 
     List getAllMovies(){
-     ArrayList<String> mvs=new ArrayList<>();
-      for(String s:movies.keySet()){
-          mvs.add(s);
-      }
-      return mvs;
+        ArrayList<String> mvs=new ArrayList<>();
+        for(String s:movies.keySet()){
+            mvs.add(s);
+        }
+        return mvs;
     }
     String deleteDirectorByName(String dirName){
-    if(directors.containsKey(dirName)){
+        if(directors.containsKey(dirName)){
 
-        if(pair.containsKey(dirName)){
-            List<String> movies1=pair.get(dirName);
-            for(String mv:movies1){
-                movies.remove(mv);
+            if(pair.containsKey(dirName)){
+                List<String> movies1=pair.get(dirName);
+                for(String mv:movies1){
+                    movies.remove(mv);
+                }
+                pair.remove(dirName);
             }
-            pair.remove(dirName);
+            directors.remove(dirName);
+            return "SUCCESSFULLY DELETED ";
         }
-        directors.remove(dirName);
-        return "SUCCESSFULLY DELETED ";
-    }
-       return null;
+        return null;
     }
 
 
     String deleteAllDirectors(){
-     ArrayList<String> list=new ArrayList<>();
-     for(String s:pair.keySet()){
-       for(String m:pair.get(s)){
-           list.add(m);
-       }
-     }
-     for(String i:list){
-         movies.remove(i);
-     }
-    return  "SUCCESSFULLY DELETED ALL DIRECTORS";
+        ArrayList<String> list=new ArrayList<>();
+        for(String s:pair.keySet()){
+            for(String m:pair.get(s)){
+                list.add(m);
+            }
+        }
+        for(String i:list){
+            movies.remove(i);
+        }
+        return  "SUCCESSFULLY DELETED ALL DIRECTORS";
     }
 
     List getMoviesByDirectorName(String dirName){
-    List<String> movieNames=new ArrayList<>();
-     if(pair.containsKey(dirName)){
+        List<String> movieNames=new ArrayList<>();
+        if(pair.containsKey(dirName)){
 
-         movieNames=(pair.get(dirName));
-     }
-     return movieNames;
+            movieNames=(pair.get(dirName));
+        }
+        return movieNames;
     }
 }
